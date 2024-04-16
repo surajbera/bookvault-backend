e6d9622152dca293b14113cee776f184896e7d13
suraj.be <suraj.be@media.net>
2024-04-16 20:55:25 +0530

    feat(env): configure environment variable access

--- server.ts
+++ server.ts
@@ -2,8 +2,7 @@ import app from './src/app';
 import { config } from './src/config/config';
 
 const startServer = () => {
-  const port = process.env.PORT || 3000;
-  console.log(config);
+  const port = config.port || 3000;
 
   app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,12 @@
+import { config as conf } from 'dotenv';
+conf();
+
+// variables starting with an underscore (_) are treated as private by developers
 const _config = {
   port: process.env.PORT,
 };
+
+console.log(process.env.PORT);
 
 // creating the config read only using Object.freeze()
 export const config = Object.freeze(_config);
