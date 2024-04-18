f08ac45ecd4fcad0d208867fc550eb489330758b
suraj.be <suraj.be@media.net>
2024-04-18 11:56:29 +0530

    delete unwanted comments and log statements

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,5 @@ const _config = {
   port: process.env.PORT,
 };
 
-console.log(process.env.PORT);
-
 // creating the config read only using Object.freeze()
 export const config = Object.freeze(_config);
