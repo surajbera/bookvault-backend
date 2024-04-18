cc430fafdfb371a43798b38a4559d3a0e223cd74
suraj.be <suraj.be@media.net>
2024-04-18 19:31:37 +0530

    add error handling to userController.ts

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import userModel from './userModel';
 import bcrypt from 'bcrypt';
 import { config } from '../config/config';
 import { sign } from 'jsonwebtoken';
+import { IUser } from './userTypes';
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // Validation
@@ -14,33 +15,45 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(customError);
   }
 
-  // Database call
-  const user = await userModel.findOne({ email: email });
-
-  if (user) {
-    const customError = createHttpError(400, 'User already exists with this email');
-    return next(customError);
+  try {
+    // Database call
+    const user = await userModel.findOne({ email: email });
+    if (user) {
+      const customError = createHttpError(400, 'User already exists with this email');
+      return next(customError);
+    }
+  } catch (error) {
+    return next(createHttpError(500, 'Error while getting user'));
   }
 
   // password hashing
   const saltRounds = Number(config.saltRounds) || 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-  // Storing the user in database
-  const newUser = await userModel.create({
-    name: name,
-    email: email,
-    password: hashedPassword,
-  });
+  let newUser: IUser;
+  try {
+    // Storing the user in database
+    newUser = await userModel.create({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return next(createHttpError(500, 'Error while getting user'));
+  }
 
   // Token Generation
-  const payload = { sub: newUser._id.toString() };
-  const secret = config.jwtSecret;
-  const options = { expiresIn: '7d' };
-  const token = sign(payload, secret as string, options); // synchronous function, no need to add await
+  try {
+    const payload = { sub: newUser._id.toString() };
+    const secret = config.jwtSecret;
+    const options = { expiresIn: '7d' };
+    const token = sign(payload, secret as string, options); // synchronous function, no need to add await
 
-  // Response
-  res.json({ accessToken: token });
+    // Response
+    res.json({ accessToken: token });
+  } catch (error) {
+    return next(createHttpError(500, 'Error while signing the JWT token'));
+  }
 };
 
 export { createUser };
