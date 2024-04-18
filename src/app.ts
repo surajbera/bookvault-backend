a2fa6d06e4729f3b53233f017dcfe9cc5a72cff7
suraj.be <suraj.be@media.net>
2024-04-18 14:57:49 +0530

    add a new middleware for handling global errors

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,13 @@
-import express, { Request, Response, NextFunction } from 'express';
-import createHttpError, { HttpError } from 'http-errors';
-import { config } from './config/config';
+import express from 'express';
+import globalErrorHandler from './middlewares/globalErrorHandler';
 
 const app = express();
 
 app.get('/', (req, res, next) => {
-  // throw new Error('Something went wrong');
-  // instead of throwing an error like above
-  // making use of the createHttpError() method
-  // from the http-errors library
-
-  const customError = createHttpError(400, 'Something went wrong');
-  throw customError;
   res.json({ message: 'Welcome to elib apis' });
 });
 
 // Global error handler
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = err.statusCode || 500;
-  return res.status(statusCode).json({
-    message: err.message,
-    errorStack: config.env === 'development' ? err.stack : '',
-  });
-  // make sure to update NODE_ENV to production
-});
+app.use(globalErrorHandler);
 
 export default app;
