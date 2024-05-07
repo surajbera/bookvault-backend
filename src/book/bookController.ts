aa9c82644c86535c8c492d3de41f8efd2fa4d02e
suraj.be <suraj.be@media.net>
2024-05-07 20:26:34 +0530

    refactor: bookController

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,20 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import path from "node:path";
+import logMessage from "../utils/logMessage";
 
 export const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  console.log("files =>", req.files);
+  logMessage("req.files", req.files);
 
   try {
+    // uploading book image
     const files = req.files as { [fieldName: string]: Express.Multer.File[] };
     const fileName = files.coverImage[0].filename;
     const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
     const filePath = path.resolve(__dirname, "../../public/data/uploads", fileName);
 
     const uploadResult = await cloudinary.uploader.upload(filePath, {
-      filename_override: fileName,
       folder: "book-covers",
       format: coverImageMimeType,
+      filename_override: fileName,
     });
 
     console.log("uploadResult => ", uploadResult);
