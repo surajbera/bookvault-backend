801d6c4816d6d3d8eccb25a6bb590c3b7e40bbac
suraj.be <suraj.be@media.net>
2024-05-07 19:47:52 +0530

    feat: file upload in clodinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,5 +1,27 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "node:path";
 
 export const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: 'Create Book Route' });
+  console.log("files =>", req.files);
+
+  try {
+    const files = req.files as { [fieldName: string]: Express.Multer.File[] };
+    const fileName = files.coverImage[0].filename;
+    const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+    const filePath = path.resolve(__dirname, "../../public/data/uploads", fileName);
+
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      filename_override: fileName,
+      folder: "book-covers",
+      format: coverImageMimeType,
+    });
+
+    console.log("uploadResult => ", uploadResult);
+
+    res.json({ message: "Create Book Route" });
+  } catch (error) {
+    console.error("Error uploading file: ", error);
+    res.status(500).json({ error: "Failed to upload image, pls try again" });
+  }
 };
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import { config as conf } from 'dotenv';
+import { config as conf } from "dotenv";
 conf();
 
 // variables starting with an underscore (_) are treated as private by developers
@@ -8,6 +8,9 @@ const _config = {
   env: process.env.NODE_ENV,
   saltRounds: process.env.SALT_ROUNDS,
   jwtSecret: process.env.JWT_SECRET,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
 };
 
 // creating the config read only using Object.freeze()
